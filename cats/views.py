1a30bb767398ec773b6fc2ab0dd77b185b8a30ac
Иван Лебедев <ivan_lbd@mail.ru>
2024-04-03 11:40:42 +0300

    update project

--- cats/views.py
+++ cats/views.py
@@ -1,34 +1,9 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import viewsets
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
-@api_view(['GET', 'POST'])
-def cat_list(request):
-    if request.method == 'POST':
-        serializer = CatSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    cats = Cat.objects.all()
-    serializer = CatSerializer(cats, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET', 'POST'])  # Разрешены только POST- и GET-запросы
-def cat_list(request):
-    # В случае POST-запроса добавим список записей в БД
-    if request.method == 'POST':
-        serializer = CatSerializer(data=request.data, many=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    # В случае GET-запроса возвращаем список всех котиков
-    cats = Cat.objects.all()
-    serializer = CatSerializer(cats, many=True)
-    return Response(serializer.data)
+class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
